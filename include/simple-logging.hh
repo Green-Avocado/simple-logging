c07d269a4c11499b496e425c3ea78ec2d98ad65e
GreenAvocado <greenavocado@protonmail.com>
2020-05-30 00:41:55 -0600

    feat: wrote basic header file

---
+++ include/simple-logging.hh
@@ -0,0 +1,11 @@
+#ifndef SIMPLELOGGING_H
+
+#include <chrono>
+#include <string>
+
+#include <../src/simple-logging.cc>
+
+std::chrono::system_clock::time_point simplelog(std::string input);
+
+#endif
+
