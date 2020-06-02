a1443fa8c32d9ca1e4a07140eb1ebbda92435403
GreenAvocado <greenavocado@protonmail.com>
2020-06-01 21:58:17 -0600

    feat: reworked library to use time since epoch

--- include/simple-logging.hh
+++ include/simple-logging.hh
@@ -1,17 +1,72 @@
 #ifndef SIMPLELOGGING_H
 
 #include <chrono>
-#include <ctime>
 #include <iostream>
+#include <ratio>
 #include <string>
 
-std::chrono::system_clock::time_point simplelog(std::string input)
+long int log_milli(std::string input)
 {
-    std::chrono::system_clock::time_point time = std::chrono::system_clock::now();
+    long int time = std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
 
-    std::time_t tt = std::chrono::system_clock::to_time_t(time);
+    std::cout << "[" << time << "] " << input << std::endl;
 
-    std::cout << ctime(&tt) << " " << input << std::endl;
+    return time;
+}
+
+long int log_milli(char* input)
+{
+    long int time = std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    std::cout << "[" << time << "] " << input << std::endl;
+
+    return time;
+}
+
+long int log_micro(std::string input)
+{
+    long int time = std::chrono::duration_cast< std::chrono::microseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    std::cout << "[" << time << "] " << input << std::endl;
+
+    return time;
+}
+
+long int log_micro(char* input)
+{
+    long int time = std::chrono::duration_cast< std::chrono::microseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    std::cout << "[" << time << "] " << input << std::endl;
+
+    return time;
+}
+
+long int log_nano(std::string input)
+{
+    long int time = std::chrono::duration_cast< std::chrono::nanoseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    std::cout << "[" << time << "] " << input << std::endl;
+
+    return time;
+}
+
+long int log_nano(char* input)
+{
+    long int time = std::chrono::duration_cast< std::chrono::nanoseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    std::cout << "[" << time << "] " << input << std::endl;
 
     return time;
 }
