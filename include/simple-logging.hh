3ddd256777b98cddc1b177f134d58fb347ea7a12
Jason Ngo <greenavocado@protonmail.com>
2021-03-11 17:44:56 -0800

    feat: reduced redundant code

--- include/simple-logging.hh
+++ include/simple-logging.hh
@@ -19,13 +19,7 @@ unsigned long long int log_milli(std::string input)
 
 unsigned long long int log_milli(char* input)
 {
-    unsigned long long int time = std::chrono::duration_cast< std::chrono::milliseconds >(
-        std::chrono::system_clock::now().time_since_epoch()
-    ).count();
-
-    std::cout << "[" << time << "] " << input << std::endl;
-
-    return time;
+    return log_milli(std::string(input));
 }
 
 unsigned long long int log_micro(std::string input)
@@ -41,13 +35,7 @@ unsigned long long int log_micro(std::string input)
 
 unsigned long long int log_micro(char* input)
 {
-    unsigned long long int time = std::chrono::duration_cast< std::chrono::microseconds >(
-        std::chrono::system_clock::now().time_since_epoch()
-    ).count();
-
-    std::cout << "[" << time << "] " << input << std::endl;
-
-    return time;
+    return log_micro(std::string(input));
 }
 
 unsigned long long int log_nano(std::string input)
@@ -63,13 +51,7 @@ unsigned long long int log_nano(std::string input)
 
 unsigned long long int log_nano(char* input)
 {
-    unsigned long long int time = std::chrono::duration_cast< std::chrono::nanoseconds >(
-        std::chrono::system_clock::now().time_since_epoch()
-    ).count();
-
-    std::cout << "[" << time << "] " << input << std::endl;
-
-    return time;
+    return log_nano(std::string(input));
 }
 
 #endif
